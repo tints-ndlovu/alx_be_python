6b967344d2f7d41454e5a2ddfa48dc7b9fa9332f
Tentswalo <tintswalotndlovu@gmail.com>
2025-07-17 15:18:32 +0200

    Personal Finance calculator

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,7 @@
+income = int(input("Enter your monthly income: "))
+expenses = int(input("Enter your monthly expenses: "))
+savings = income - expenses
+projected_savings = savings * 12 + (savings * 12 * 0.05)
+
+print(f"Your monthly savings are {savings}.")
+print(f"Projected savings after one year, with interest, is: {projected_savings}.")
