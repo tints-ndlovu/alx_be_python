c8042124559886d5e6e7af00b74a500c85721668
Tentswalo <tintswalotndlovu@gmail.com>
2025-07-19 10:02:15 +0200

    Weather recommendation program

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,10 @@
+response = input(" What's the weather like today? (sunny/rainy/cold): ")
+
+if (response == "sunny"):
+    print("Wear a t-shirt and sunglasses.")
+elif (response == "rainy"):
+    print("Don't forget your umbrella and a raincoat.")
+elif (response == "cold"):
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
