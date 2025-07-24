ba0a05ca5ddac29ae7f751965ca1b0876c6d9d05
Tentswalo <tintswalotndlovu@gmail.com>
2025-07-24 19:11:14 +0200

    Arithmetic operations adding elif

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,7 +9,7 @@ def perform_operation(num1, num2, operation):
         case "divide":
             if num2 == 0:
                 result = "Cannot divide by 0"
-            else:
+            elif num2 != 0 :
                 result = num1 / num2
     return result
 
