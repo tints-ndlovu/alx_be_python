5dc815b4115fd82afe5f87309381b5ebc89ee346
Tentswalo <tintswalotndlovu@gmail.com>
2025-07-24 18:17:41 +0200

    Arithmetic operations

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,15 @@
+def perform_operation(num1, num2, operation):
+    match operation:
+        case "add":
+            result = num1 + num2
+        case "subtract":
+            result = num1 - num2
+        case "multiply":
+            result = num1 * num2
+        case "divide":
+            if num2 == 0:
+                result = "Cannot divide by 0"
+            else:
+                result = num1 / num2
+    return result
+
