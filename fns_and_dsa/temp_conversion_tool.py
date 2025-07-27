fd4349c5d4704762665d20645477d686bf5b7a9c
Tentswalo <tintswalotndlovu@gmail.com>
2025-07-27 22:25:10 +0200

    Fixing conversion calculator

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,11 +2,11 @@ FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
-    converted = fahrenheit * FAHRENHEIT_TO_CELSIUS_FACTOR
+    converted = fahrenheit * FAHRENHEIT_TO_CELSIUS_FACTOR -32
     return converted
 
 def convert_to_fahrenheit(celsius):
-    converted = celsius * CELSIUS_TO_FAHRENHEIT_FACTOR
+    converted = celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32
     return converted
 
 temperature_int = int(input("Enter the temperature to convert: "))
