80e52690ed1e057176d7d29df2be36138676df9b
Tentswalo <tintswalotndlovu@gmail.com>
2025-07-27 20:14:25 +0200

    Imported datetime

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,11 +1,12 @@
-import datetime
+from datetime import datetime
 
 def display_current_datetime():
-    current_date = datetime.datetime.now()
+    current_date = datetime.now()
     print(current_date)
 
 number_of_days = int(input("Enter a number of days (as an integer): "))
 
+
 def calculate_future_date():
-    future_date = datetime.datetime.now() + datetime.timedelta(days=number_of_days)
+    future_date = datetime.now() + datetime.timedelta(days=number_of_days)
     print(future_date)
