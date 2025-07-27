1584e669f6348f5d77aae15d26259639d877a66b
Tentswalo <tintswalotndlovu@gmail.com>
2025-07-27 21:02:56 +0200

    Fixed number of days to add prompt

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -5,7 +5,7 @@ def display_current_datetime():
     current_date = datetime.now()
     print(current_date.strftime("%Y-%m-%d %H:%M:%S"))
 
-number_of_days = int(input("Enter a number of days (as an integer): "))
+number_of_days = int(input("Enter the number of days to add to the current date: "))
 
 
 def calculate_future_date():
