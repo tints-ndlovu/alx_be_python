c204982e7ecbf1f2850c650ab3912f1529b54519
Tentswalo <tintswalotndlovu@gmail.com>
2025-07-27 20:19:05 +0200

    Added format for dates

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 
 def display_current_datetime():
-    current_date = datetime.now()
+    current_date = datetime.now("%Y-%m-%d %H:%M:%S")
     print(current_date)
 
 number_of_days = int(input("Enter a number of days (as an integer): "))
 
 
 def calculate_future_date():
-    future_date = datetime.now() + datetime.timedelta(days=number_of_days)
+    future_date = datetime.now("%Y-%m-%d %H:%M:%S") + datetime.timedelta(days=number_of_days)
     print(future_date)
