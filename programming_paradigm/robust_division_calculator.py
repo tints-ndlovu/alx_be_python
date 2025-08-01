d28364830107e81afbe128c5b49b2c987e7006d9
Tentswalo <tintswalotndlovu@gmail.com>
2025-08-01 07:34:07 +0200

    Fixed Value error message

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -6,6 +6,6 @@ def safe_divide(numerator, denominator):
     except ZeroDivisionError:
         print("Error: Cannot divide by zero.")
     except ValueError:
-        print("Please enter numeric values only.")
+        print("Error: Please enter numeric values only.")
     
 
